f4dd970cd6aa86c8283e25a72700188c916b97bc
Luiz Pires <lpiresjunior@gmail.com>
2021-06-11 04:58:54 -0300

    Add Header on Dashboard

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,11 +1,34 @@
 import React from 'react';
 
-import { Container, Title } from './styles';
+import {
+  Container,
+  Header,
+  UserWrapper,
+  UserInfo,
+  Photo,
+  User,
+  UserGrading,
+  UserName
+} from './styles';
 
 export function Dashborad() {
   return (
     <Container>
-      <Title>Dashboard</Title>
+      <Header>
+        <UserWrapper>
+          <UserInfo>
+            <Photo
+              source={{
+                uri: 'https://avatars.githubusercontent.com/u/59424748?v=4'
+              }}
+            />
+            <User>
+              <UserGrading>Olá,</UserGrading>
+              <UserName>Luiz</UserName>
+            </User>
+          </UserInfo>
+        </UserWrapper>
+      </Header>
     </Container>
   )
 }
