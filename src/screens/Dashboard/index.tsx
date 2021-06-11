272433bac4bdc639d16131a8833dd4844420743b
Luiz <luiz@Luizs-iMac.local>
2021-06-11 03:47:15 -0300

    Add styled components

---
+++ src/screens/Dashboard/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Container, Title } from './styles';
+
+export function Dashborad() {
+  return (
+    <Container>
+      <Title>Dashboard</Title>
+    </Container>
+  )
+}
