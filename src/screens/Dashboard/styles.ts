6a7dc4b267ab54873518f9ebbacadfc8107f51c0
Luiz Pires <lpiresjunior@gmail.com>
2021-06-11 04:05:10 -0300

    Add fonts

--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -4,11 +4,11 @@ export const Container = styled.View`
   flex: 1;
   justify-content: center;
   align-items: center;
-  background-color: ${({ theme }) => theme.colors.background };
+  background-color: ${({ theme }) => theme.colors.background};
 `;
 
 export const Title = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.bold};
   font-size: 24px;
-  font-weight: bold;
-  color: ${({ theme }) => theme.colors.title };
+  color: ${({ theme }) => theme.colors.title};
 `;
