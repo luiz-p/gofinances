a355ac9efd116edc7e32e656cde233426d460fb2
Luiz Pires <lpiresjunior@gmail.com>
2021-06-19 10:20:04 -0300

    Add routes

--- App.tsx
+++ App.tsx
@@ -10,9 +10,10 @@ import {
   Poppins_700Bold,
   useFonts,
 } from '@expo-google-fonts/poppins'
+import { NavigationContainer } from '@react-navigation/native'
 
 import theme from './src/global/styles/theme'
-import { Register } from './src/screens/Register'
+import { AppRoutes } from './src/routes/app.routes'
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -27,7 +28,9 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <Register />
+      <NavigationContainer>
+        <AppRoutes />
+      </NavigationContainer>
     </ThemeProvider>
   )
 }
