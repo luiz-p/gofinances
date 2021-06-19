8dd4575e74660f19c117009323375b51544e51f3
Luiz Pires <lpiresjunior@gmail.com>
2021-06-19 10:10:20 -0300

    Install react nativation

--- App.tsx
+++ App.tsx
@@ -1,12 +1,14 @@
-import React from 'react'
+import 'react-native-gesture-handler'
+
 import AppLoading from 'expo-app-loading'
+import React from 'react'
 import { ThemeProvider } from 'styled-components'
 
 import {
-  useFonts,
   Poppins_400Regular,
   Poppins_500Medium,
   Poppins_700Bold,
+  useFonts,
 } from '@expo-google-fonts/poppins'
 
 import theme from './src/global/styles/theme'
